b2f52376ae2673c0740efc49c66e33ceb5007048
Tomasz <tomaush02@gmail.com>
2023-05-22 18:12:08 +0200

    test

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ public:
     bool OnInit() {
         wxFrame* window = new wxFrame(NULL, wxID_ANY, "GUI Test", wxDefaultPosition, wxSize(600, 400));
         wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
-        wxStaticText* text = new wxStaticText(window, wxID_ANY, "Well Done!\nEverything seems to be working",
+        wxStaticText* text = new wxStaticText(window, wxID_ANY, "dla radka chuja",
             wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE_HORIZONTAL);
         text->SetFont(wxFont(20, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
         sizer->Add(text, 1, wxALIGN_CENTER);
